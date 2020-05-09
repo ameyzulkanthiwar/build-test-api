eab25ccf1a5494ebc04dff46b8c58f0b2d3e3a60
Amey Zulkanthiwar <amey@patientfinder.co>
2020-05-09 12:37:59 -0500

    Connect the mongodb

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+/**
+ * Middleware
+ * It's a function and it get execute when particular routes are get hit
+ * Example: app.use(specify_route, function)
+ */
 
 // Routes
 app.get("/", (req, res) => {
     res.send("Server just started!");
 });
 
-// Start the listening to server
-// or may say boot up the server
+app.get("/post", (req, res) => {
+    res.send("We are on the post!");
+});
+
+// Connect to MongoDB
+mongoose.connect(
+    process.env.db_connection_url,
+    { useUnifiedTopology: true, useNewUrlParser: true }, // Was getting warning in the console
+    () => {
+        console.log("Connected to DB!");
+    }
+);
+
+/**
+ * Start the listening to server or may say boot up the server
+ */
 app.listen(5000);
